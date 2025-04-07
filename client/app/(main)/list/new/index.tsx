29088a64e75b58be0fd4b0b07bc1129fd65d5fa8
Arthur Quinellato <arthur.quinellato@gmail.com>
2025-04-07 16:09:55 -0300

    Moved project into client

---
+++ client/app/(main)/list/new/index.tsx
@@ -0,0 +1,11 @@
+import { Div } from '@/components/DynamicInterfaceView'
+import { Text } from '@/components/ThemedText'
+import React from 'react'
+
+export default function NewListScreen() {
+	return (
+		<Div>
+			<Text>Create list</Text>
+		</Div>
+	)
+}
